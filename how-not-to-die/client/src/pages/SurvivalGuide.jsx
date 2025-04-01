c79e2e8fc17a289322ef5ddfb3e548e17074dd69
zomblic <zomblic@hotmail.com>
2025-04-01 17:58:46 -0400

    survivial guide code

--- how-not-to-die/client/src/pages/SurvivalGuide.jsx
+++ how-not-to-die/client/src/pages/SurvivalGuide.jsx
@@ -1,12 +1,32 @@
 // file path: how-not-to-die/client/src/pages/SurvivalGuide.jsx
 // Display safety and exploration tips
 
-import React from 'react';
+import React, { use } from 'react';
 import { Link } from 'react-router-dom';
 import styles from '../assets/css/survival-guide/SurvivalGuide.module.css';
 import AnimatedVeraQuote from '../components/vera-quotes/AnimatedVeraQuote';
 
 const SurvivalGuide = () => {
+  const [planets, setPlanets] = useState([]);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    const fetchPlanets = async () => {
+      try {
+        const response = await fetch('/api/planets');
+        if (!response.ok) {
+          throw new Error('No planets, no go');
+        }
+        const data = await response.json();
+        setPlanets(data);
+      } catch (error) {
+        setError(err.message);
+      }
+    };
+
+    fetchPlanets();
+  }, []);
+
   return (
     <div className={styles.container}>
       <h2>Survival Guide</h2>
@@ -22,6 +42,42 @@ const SurvivalGuide = () => {
         <li>Keep personal logs updated. Posthumous documentation earns extra hazard pay.</li>
         <li>Never trust a plant with teeth. This should be obvious.</li>
         <li>Recharge your suit before nightfall. Nightfall is... bad.</li>
+      </ul>
+      {/* Display planets dynamically */}
+      <ul className={styles.tips}>
+        {planets.map((planet) => (
+          <li key={planet.id}>
+            <strong>{planet.name}</strong>: {planet.description || 'Data Corrupted.'}
+          </li>
+        ))}
+      </ul>
+      <ul className={styles.tips}>
+        {planets.map((landingspot) => (
+          <li key={landingspot.id}>
+            <strong>{landingspot.name}</strong>: {landingspot.description || 'No Corrupted available.'}
+          </li>
+        ))}
+      </ul>
+      <ul className={styles.tips}>
+        {planets.map((flora) => (
+          <li key={flora.id}>
+            <strong>{flora.name}</strong>: {flora.description || 'No Corrupted available.'}
+          </li>
+        ))}
+      </ul>
+      <ul className={styles.tips}>
+        {planets.map((fauna) => (
+          <li key={fauna.id}>
+            <strong>{fauna.name}</strong>: {fauna.description || 'No description available.'}
+          </li>
+        ))}
+      </ul>
+      <ul className={styles.tips}>
+        {planets.map((unexplained) => (
+          <li key={unexplained.id}>
+            <strong>{unexxplained.name}</strong>: {unexplained.description || 'No description available.'}
+          </li>
+        ))}
       </ul>
 
       <div className={styles.logButtons}>

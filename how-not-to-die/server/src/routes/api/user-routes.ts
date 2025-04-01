aed9ca6aeb8f0f4093b2d456e11ce17885204aa5
Alex Menendez <alexis.menendez@austincc.edu>
2025-04-01 14:05:30 -0500

    Update user-routes.ts

--- how-not-to-die/server/src/routes/api/user-routes.ts
+++ how-not-to-die/server/src/routes/api/user-routes.ts
@@ -1,3 +1,5 @@
+//file path: HowNotToDieDemo/how-not-to-die/server/src/routes/api/user-routes.ts
+
 import express from 'express';
 import User from '../../models/user.js';
 import { verifyToken } from '../../middleware/auth.js';

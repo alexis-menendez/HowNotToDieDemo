0267c6c8a4f8bc36e9ae1465728908e2ad254233
Alexis Menendez <alexis.menendez@austincc.edu>
2025-04-03 00:02:15 -0500

    adjusting ports

--- how-not-to-die/client/src/components/home/LoginForm.jsx
+++ how-not-to-die/client/src/components/home/LoginForm.jsx
@@ -22,7 +22,7 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await fetch('/auth/login', {
+      const response = fetch(`${import.meta.env.VITE_API_BASE_URL}/auth/login`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password }),
